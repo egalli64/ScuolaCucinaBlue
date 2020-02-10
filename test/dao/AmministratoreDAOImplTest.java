7ab82e27fcabf130a62e6a7e41b8dc3603535947
Lucrezia Rovai <lucrezia.rovai@hotmail.it>
2020-02-10 12:26:07 +0100

    test added

---
+++ test/dao/AmministratoreDAOImplTest.java
@@ -0,0 +1,14 @@
+package dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AmministratoreDAOImplTest {
+
+	@Test
+	void testSelect() {
+		fail("Not yet implemented");
+	}
+
+}
