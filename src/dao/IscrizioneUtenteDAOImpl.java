59136454ac5f183f15e08322c8d58d4aab59e615
Lucrezia Rovai <lucrezia.rovai@hotmail.it>
2020-02-11 16:34:31 +0100

    arci commit

--- src/dao/IscrizioneUtenteDAOImpl.java
+++ src/dao/IscrizioneUtenteDAOImpl.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -14,54 +15,97 @@ public class IscrizioneUtenteDAOImpl implements IscrizioneUtenteDAO {
 
 	private Connection conn;
 
-	public IscrizioneUtenteDAOImpl() throws ConnessioneException{
+	public IscrizioneUtenteDAOImpl() throws ConnessioneException {
 		conn = SingletonConnection.getInstance();
 	}
-	
+
 	/*
-	 * iscrizione di un certo utente ad una certa edizione di un corso.
-	 * sia l'utente che l'edizione devono già essere stati registrati in precedenza
-	 * se l'utente e/o l'edizione non esistono o l'utente è già iscritto a quella edizione si solleva una eccezione
+	 * iscrizione di un certo utente ad una certa edizione di un corso. sia l'utente
+	 * che l'edizione devono già essere stati registrati in precedenza se l'utente
+	 * e/o l'edizione non esistono o l'utente è già iscritto a quella edizione si
+	 * solleva una eccezione
 	 */
 	@Override
 	public void iscriviUtente(int idEdizione, String idUtente) throws SQLException {
 
-		
-		PreparedStatement ps=conn.prepareStatement("insert into iscritti(id_edizione, id_utente) values (?,?)");
-		ps.setInt(1,idEdizione);
-		ps.setString (2, idUtente);
+		PreparedStatement ps = conn.prepareStatement("insert into iscritti(id_edizione, id_utente) values (?,?)");
+		ps.setInt(1, idEdizione);
+		ps.setString(2, idUtente);
+		ps.executeUpdate();
 
 	}
 
 	/*
-	 * cancellazione di una iscrizione ad una edizione
-	 * nota: quando si cancella l'iscrizione, sia l'utente che l'edizione non devono essere cancellati
-	 * se l'utente e/o l'edizione non esistono si solleva una eccezione
+	 * cancellazione di una iscrizione ad una edizione nota: quando si cancella
+	 * l'iscrizione, sia l'utente che l'edizione non devono essere cancellati se
+	 * l'utente e/o l'edizione non esistono si solleva una eccezione
 	 */
 	@Override
 	public void cancellaIscrizioneUtente(int idEdizione, String idUtente) throws SQLException {
-		// TODO Auto-generated method stub
 
+		PreparedStatement ps = conn.prepareStatement("DELETE FROM iscritti WHERE id_utente=?");
+		ps.setString(1, idUtente);
+		int n = ps.executeUpdate();
+		if (n == 0)
+			throw new SQLException("utente " + idUtente + " non presente");
 	}
 
 	/*
-	 * lettura di tutte le edizioni a cui è iscritto un utente
-	 * se l'utente non esiste o non è iscritto a nessuna edizione si torna una lista vuota
+	 * lettura di tutte le edizioni a cui è iscritto un utente se l'utente non
+	 * esiste o non è iscritto a nessuna edizione si torna una lista vuota
 	 */
 	@Override
 	public ArrayList<Edizione> selectIscrizioniUtente(String idUtente) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+
+		ArrayList<Edizione> edizioni = new ArrayList<Edizione>();
+
+		PreparedStatement ps = conn.prepareStatement(
+				"SELECT * FROM calendario join iscritti using (id_edizione) join registrati using (id_utente =?)");
+
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			int idEdizione = rs.getInt("id_edizione");
+			int idCorso = rs.getInt("id_corso");
+			Date dataInizio = rs.getDate("dataInizio");
+			int durata = rs.getInt("durata");
+			String aula = rs.getString("aula");
+			String docente = rs.getString("docente");
+
+			Edizione edizione = new Edizione(idEdizione, idCorso, dataInizio, durata, aula, docente);
+			edizioni.add(edizione);
+
+		}
+
+		return edizioni;
 	}
-	
+
 	/*
-	 * lettura di tutti gli utenti iscritti ad una certa edizione
-	 * se l'edizione non esiste o non vi sono utenti iscritti si torna una lista vuota
+	 * lettura di tutti gli utenti iscritti ad una certa edizione se l'edizione non
+	 * esiste o non vi sono utenti iscritti si torna una lista vuota
 	 */
 	@Override
 	public ArrayList<Utente> selectUtentiPerEdizione(int idEdizione) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+
+		ArrayList<Utente> corsisti = new ArrayList<Utente>();
+		PreparedStatement ps = conn.prepareStatement(
+				"SELECT * FROM calendario join iscritti using (id_edizione=?) join registrati using (id_utente)");
+
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			String idUtente = rs.getString("id_utente");
+			String password = rs.getString("password");
+			String nome = rs.getString("nome");
+			String cognome = rs.getString("cognome");
+			Date dataNascita = rs.getDate("dataNascita");
+			String email = rs.getString("email");
+			String telefono = rs.getString("telefono");
+
+			Utente corsista = new Utente(idUtente, password, nome, cognome, dataNascita, email, telefono, false);
+
+			corsisti.add(corsista);
+		}
+
+		return corsisti;
 	}
 
 	/*
@@ -69,8 +113,12 @@ public class IscrizioneUtenteDAOImpl implements IscrizioneUtenteDAO {
 	 */
 	@Override
 	public int getNumeroIscritti(int idEdizione) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+
+		PreparedStatement ps = conn.prepareStatement(
+				"SELECT COUNT(nome) FROM calendario join iscritti using (id_edizione=?) join registrati using (id_utente)");
+		int counter = ps.executeUpdate();
+
+		return counter;
 	}
 
 }
